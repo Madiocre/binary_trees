799d49d29bd184339cd1c7a9b56dbd259fd715c1
Madiocre <madiocre.x@gmail.com>
2024-02-29 14:57:05 -0800

    fixed balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+size_t binary_tree_h(const binary_tree_t *tree);
 /**
  * binary_tree_balance - Measures the balance factor.
  * @tree: A pointer to the root node.
@@ -22,14 +23,14 @@ int binary_tree_balance(const binary_tree_t *tree)
  * Return: If tree is NULL, your function must return 0
  *         else return height.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_h(const binary_tree_t *tree)
 {
 	if (tree)
 	{
 		size_t l = 0, r = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		l = tree->left ? 1 + binary_tree_h(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_h(tree->right) : 1;
 		return ((l > r) ? l : r);
 	}
 	return (0);
